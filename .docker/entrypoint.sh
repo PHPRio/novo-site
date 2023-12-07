715b56da0126670aacef91d48a35e9e283701e19
Giovanni Martins <giovannimartins2000@gmail.com>
2023-12-06 21:23:58 -0300

    atualiza .docker/entrypoint.sh
    Co-authored-by: Lucas Azevedo <lhs_azevedo@hotmail.com>

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,8 +4,8 @@
 usermod --non-unique --uid "${HOST_UID}" www-data
 groupmod --non-unique --gid "${HOST_GID}" www-data
 
-php /var/www/html/vendor/bin/jigsaw serve --host=0.0.0.0
-
 if [ ! -d "vendor" ] then
   composer install
 fi
+
+php /var/www/html/vendor/bin/jigsaw serve --host=0.0.0.0
